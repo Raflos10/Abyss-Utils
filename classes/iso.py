461f8360e9533bb43b3090859e734fdf52fdda98
Raflos <raflos@gmx.com>
2023-12-13 17:40:20 -0800

    added get_file_bytes to iso.py

--- classes/iso.py
+++ classes/iso.py
@@ -3,8 +3,16 @@ import pycdlib
 
 class Iso:
     def __init__(self, data):
-        pycd = pycdlib.PyCdlib()
+        self.pycd = pycdlib.PyCdlib()
+        self.pycd.open_fp(io.BytesIO(data))
 
-        pycd._open_fp(io.BytesIO(data))
-        for child in pycd.list_children(iso_path='/'):
-            print(child.file_identifier())
+        self.rootFileNames = []
+        for f in self.pycd.list_children(iso_path='/'):
+            if f.is_file():
+                self.rootFileNames.append(f.file_identifier().decode('utf-8'))
+
+    def get_file_bytes(self, fileName):
+        extractedData = io.BytesIO()
+        self.pycd.get_file_from_iso_fp(extractedData, iso_path='/' + fileName)
+        extractedData.seek(0)
+        return extractedData.read()
